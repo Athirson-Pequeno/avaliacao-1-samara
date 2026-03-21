8da57df645ec8578b343a3bed9046b90d6adc244
Athirson <athirsondev@gmail.com>
2026-03-21 15:35:08 -0300

    feat: Validações do turmasController

--- controllers/turmaController.js
+++ controllers/turmaController.js
@@ -1,43 +1,86 @@
-const Turma = require("../models/turma")
+const Turma = require("../models/turma");
+const errorResponse = require("../utils/errorResponse");
 
 const criarTurma = async (req, res) => {
-  const { nome, alunosIds, professorId } = req.body;
+  try {
+    const { nome, alunosIds, professorId } = req.body;
 
-  const novaTurma = new Turma({
-    nome,
-    alunos: alunosIds,
-    professor: professorId,
-  });
+    if (!nome || !professorId) {
+      return errorResponse(res, "Nome e professorId são obrigatórios", 400);
+    }
 
-  await novaTurma.save();
+    const novaTurma = new Turma({
+      nome,
+      alunos: Array.isArray(alunosIds) ? alunosIds : [],
+      professor: professorId,
+    });
 
-  res.json({
-    message: "Turma criada com sucesso!",
-    turma: novaTurma,
-  });
+    await novaTurma.save();
+
+    return res.status(201).json({
+      message: "Turma criada com sucesso!",
+      turma: novaTurma,
+    });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const obterTodasTurmas = async (req, res) => {
-  const turmas = await Turma.find().populate('alunos professor');
-  res.json(turmas);
+  try {
+    const turmas = await Turma.find().populate("alunos professor");
+    return res.status(200).json(turmas);
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const deletarTurma = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Turma.deleteOne({ _id: id });
-  res.json({ message: "Turma removida com sucesso!" });
+    const resultado = await Turma.deleteOne({ _id: id });
+
+    if (resultado.deletedCount === 0) {
+      return errorResponse(res, "Turma não encontrada", 404);
+    }
+
+    return res.status(200).json({ message: "Turma removida com sucesso!" });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const editarTurma = async (req, res) => {
-  const { id } = req.params;
-  const { nome, alunosIds, professorId } = req.body;
+  try {
+    const { id } = req.params;
+    const { nome, alunosIds, professorId } = req.body;
 
-  let turma = await Turma.findByIdAndUpdate(id, { nome, alunos: alunosIds, professor: professorId });
-  res.status(200).json({
-    message: "Turma atualizada com sucesso!",
-    turma,
-  });
+    if (!nome || !professorId) {
+      return errorResponse(res, "Nome e professorId são obrigatórios", 400);
+    }
+
+    const turma = await Turma.findByIdAndUpdate(
+      id,
+      {
+        nome,
+        alunos: Array.isArray(alunosIds) ? alunosIds : [],
+        professor: professorId,
+      },
+      { new: true }
+    );
+
+    if (!turma) {
+      return errorResponse(res, "Turma não encontrada", 404);
+    }
+
+    return res.status(200).json({
+      message: "Turma atualizada com sucesso!",
+      turma,
+    });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 module.exports = { criarTurma, editarTurma, deletarTurma, obterTodasTurmas }
